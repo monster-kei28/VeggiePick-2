73e0526f7a8a00bcfc6e6dedbaf8c3362afd346a
瀬上圭介 <segamikeisuke@segamikeisukenoMacBook-Air.local>
2025-12-01 22:35:12 +0900

    update:フォームの入力値をDBに保存

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,17 +4,30 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    # ① User を作成（または既存の電話番号ユーザーがいれば取得）
+    user = User.find_or_create_by(phone_number: params[:reservation][:phone_number]) do |u|
+      u.name = params[:reservation][:name]
+    end
+
+    # ② Reservation に紐付ける
     @reservation = Reservation.new(reservation_params)
+    @reservation.user_id = user.id
+
+    # ③ 保存
     if @reservation.save
-      redirect_to root_path
+      redirect_to root_path, notice: "予約が完了しました"
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
   def reservation_params
-    params.require(:reservation).permit(:user_id, :harvest_experience_id, :number_of_people, :reserved_at)
+    params.require(:reservation).permit(
+      :harvest_experience_id,
+      :number_of_people,
+      :reserved_at
+    )
   end
 end
